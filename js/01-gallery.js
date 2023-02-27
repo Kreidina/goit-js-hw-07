f51c5db48d1dc3f8920980ce4c4e3458d683eb8a
Anna Kreidina <kav99mail@gmail.com>
2023-02-27 14:43:43 +0200

    виконує дз 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryItemsMarcup = createGalleryItems(galleryItems);
+
+function createGalleryItems(items){
+    return items.map(({ preview, original, description}) => {
+        return (
+        `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div> `);
+    })
+    .join('');
+}
+galleryRef.innerHTML = galleryItemsMarcup;
+
+
+galleryRef.addEventListener('click', onClick);
+
+let instance = null;
+
+function onClick(evt){
+    evt.preventDefault();
+    if(evt.target.nodeName !== 'IMG'){
+        return;
+    }
+    instance = basicLightbox.create(`
+    <img src=${evt.target.dataset.source}>
+`);
+
+instance.show();
+document.addEventListener('keydown', onKeyDouwn);
+
+
+function onKeyDouwn(evt) {
+  if(evt.code === 'Escape' && instance !== 'null'){
+    instance.close();
+    document.removeEventListener('keydown', onKeyDouwn);
+  }
+}
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const marcup = createMarcupGallery(galleryItems);
+
+function createMarcupGallery(item){
+    return item.map(({ preview, original, description}) =>{
+        return `<a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+      </a>`
+    })
+    .join('')
+}
+galleryEl.innerHTML = marcup;
+
+
+galleryEl.addEventListener('click', onClick);
+
+let imgEl;
+
+function onClick(evt){
+    evt.preventDefault();
+    if(evt.target.nodeName !== 'IMG'){
+        return;
+    }
+    imgEl = evt.target;
+    gallery.show();
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    captionType: 'attr',
+    captionsData: 'alt',
+});
+
+
